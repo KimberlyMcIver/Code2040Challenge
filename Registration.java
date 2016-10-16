ebdbf0eaf0dc6fbccc8f4f9996ebd7a3a238201e
Kim McIver <kimberly_mciver@yahoo.com>
2016-10-15 22:39:15 -0700

    Update & Clean Up

--- Registration.java
+++ Registration.java
@@ -5,52 +5,58 @@ import java.net.HttpURLConnection;
 import java.io.InputStreamReader;
 import java.net.URL;
      
-        
+// STEP 1: REGISTRATION
+
 public class Registration {
+    
     private final String tokenValue = "aa8874426685ac9674ccf75518f60ec0";
-    
-    public static void main(String[] args) throws Exception {
-    
-        Registration http = new Registration();
-        http.request();
-    }
      
     private void request() throws Exception {
     
-        String url = "http://challenge.code2040.org/api/register";
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        
-        con.setRequestMethod("POST");
-        con.setRequestProperty("token", tokenValue);
-        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-        con.setRequestProperty("Content-Type", "application/json");
-        
-        String myJsonData = 
-                "{\"token\":\"aa8874426685ac9674ccf75518f60ec0\",\"github\":\"https://github.com/KimberlyMcIver/Code2040Challenge.git\"}";
-        
-        con.setDoOutput(true);
-        try (DataOutputStream request = new DataOutputStream(con.getOutputStream())) {
-            request.writeBytes(myJsonData);
-            request.flush();
-        }
-        
-        int outputCode = con.getResponseCode();
-        System.out.println("\nSending 'POST' request to URL : " + url);
-        System.out.println("Post Info: " + myJsonData);
-        System.out.println("Output Code: " + outputCode);
-        
-        StringBuffer outputData;
-        try (BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream())
-        )) {
-            String output;
-            outputData = new StringBuffer();
-            while ((output = in.readLine()) != null) {
-                outputData.append(output);
+        String endpoint = "http://challenge.code2040.org/api/register";
+        URL obj = new URL(endpoint);
+        HttpURLConnection newConnection = (HttpURLConnection) obj.openConnection();
+        newConnection.setRequestMethod("POST");
+        newConnection.setRequestProperty("token", tokenValue);
+        newConnection.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+        newConnection.setRequestProperty("Content-Type", "application/json");
+        String myJsonData = "{\"token\":\"aa8874426685ac9674ccf75518f60ec0\",\"github\":\"https://github.com/KimberlyMcIver/Code2040Challenge.git\"}";
+        newConnection.setDoOutput(true);
+            
+            try (DataOutputStream request = new DataOutputStream(newConnection.getOutputStream())) {
+                request.writeBytes(myJsonData);
+                request.flush();
             }
-        }
+        
+        int outputCode = newConnection.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + endpoint);
+        System.out.println("Posting: " + myJsonData);
+        System.out.println("Output Code: " + outputCode);
+        StringBuffer outputData;
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(newConnection.getInputStream()))) {
+                String output;
+                outputData = new StringBuffer();
+            
+                while ((output = in.readLine()) != null) {
+                    outputData.append(output);
+                }
+            }
         System.out.println(outputData.toString());
     }
-            
+    
+    public static void main(String[] args) throws Exception {
+    
+        Registration step1 = new Registration();
+        step1.request();
+        
+        ReverseMe step2 = new ReverseMe();
+        step2.request();
+        
+        NeedleInHaystack1 step3 = new NeedleInHaystack1();
+        step3.request();
+        
+        Prefix step4 = new Prefix();
+        step4.request();
+    }         
 }
+
